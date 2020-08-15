7fb66556ac84ac47f392cdb888d36fbd11586380
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-14 22:33:39 -0400

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 4..20 }, 
+  uniqueness: true
+  
+  validates :email, presence: true
+  
+  validates :password, presence: true, length: { in: 6..20 }
 end
